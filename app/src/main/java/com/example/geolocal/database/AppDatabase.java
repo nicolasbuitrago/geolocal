0d2d225fd8c48a321675234819ed1a0af8e3b187
nicolasbuitrago <nicolasbuitrago@uninorte.edu.co>
2019-09-22 20:41:28 -0500

    Added Room

---
+++ app/src/main/java/com/example/geolocal/database/AppDatabase.java
@@ -0,0 +1,12 @@
+package com.example.geolocal.database;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+import com.example.geolocal.dao.UserDao;
+import com.example.geolocal.data.User;
+
+@Database(entities = {User.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+    public abstract UserDao UserDao();
+}
